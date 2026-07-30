7997c609b79fc5db9e7d17a04f11360cd2c2c556
prshnt273 <prshnt273@gmail.com>
2026-07-31 01:11:12 +0545

    Fix database query during docker build

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,27 +3,22 @@
 namespace App\Providers;
 
 use App\Models\Setting;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        if (Schema::hasTable('settings')) {
-            View::share('setting', Setting::first());
+        if (app()->runningInConsole()) {
+            return;
         }
+
+        View::share('setting', Setting::first());
     }
 }
